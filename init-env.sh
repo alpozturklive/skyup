a78b67fd59113ab483df936545a4349da857cec0
alpozturklive <alpozturklive@gmail.com>
2025-12-17 21:41:09 +0000

    compose file and init-env script for podman

--- init-env.sh
+++ init-env.sh
@@ -13,16 +13,29 @@ SQL_FILE="initdb/01-postgres-init.sql"
 #################################
 
 cat > "$ENV_FILE" <<EOF
+
+#librechat
+MONGO_ROOT_USER=admin
+MONGO_ROOT_PASSWORD=$(gen_pass)
+MONGO_DB=LibreChat
+JWT_SECRET=$(gen_pass)
+JWT_REFRESH_SECRET=$(gen_pass)
+SESSION_SECRET=$(gen_pass)
+CREDS_KEY=$(gen_pass)
+CREDS_IV=$(gen_pass|head -c 16)
+LIBRECHAT_ADMIN_EMAIL=admin@skyup.online
+LIBRECHAT_ADMIN_PASSWORD=$(gen_pass)
+ALLOW_REGISTRATION=false
+ALLOW_SOCIAL_LOGIN=false
+ALLOW_EMAIL_LOGIN=true
+APP_TITLE=LibreChat
+DOMAIN_CLIENT=http://localhost:3080
+DOMAIN_SERVER=http://localhost:3080
+
 # PostgreSQL
 POSTGRES_USER=postgres
 POSTGRES_PASSWORD=$(gen_pass)
 POSTGRES_DB=main
-
-# SimStudio
-SIM_DB=sim
-SIM_DB_USER=sim_user
-SIM_DB_PASSWORD=$(gen_pass)
-NEXT_PUBLIC_APP_URL=https://sim.skyup.online
 
 # n8n
 N8N_DB=n8n
@@ -31,20 +44,12 @@ N8N_DB_PASSWORD=$(gen_pass)
 N8N_BASIC_USER=admin
 N8N_BASIC_PASSWORD=$(gen_pass)
 
-# MongoDB
-MONGO_ROOT_USER=mongo
-MONGO_ROOT_PASSWORD=$(gen_pass)
-MONGO_DB=librechat
+# sim
+SIM_DB=sim
+SIM_DB_USER=sim_user
+SIM_DB_PASSWORD=$(gen_pass)
+NEXT_PUBLIC_APP_URL=https://sim.skyup.online
 
-# LibreChat admin
-LIBRECHAT_ADMIN_EMAIL=admin@skyup.online
-LIBRECHAT_ADMIN_PASSWORD=Password123
-JWT_SECRET=$(gen_pass)
-JWT_REFRESH_SECRET=$(gen_pass)
-SESSION_SECRET=$(gen_pass)
-ALLOW_REGISTRATION=false
-ALLOW_SOCIAL_LOGIN=false
-OLLAMA_BASE_URL=http://ollama:11434
 EOF
 
 chmod 600 "$ENV_FILE"
