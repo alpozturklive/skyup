fc5122fd5bb5ef43ba0e771205867fee104d4370
alpozturklive <alpozturklive@gmail.com>
2025-12-21 09:37:40 +0000

    permissin changed

--- initdb/01-create-extra-dbs.sql
+++ initdb/01-create-extra-dbs.sql
@@ -2,7 +2,11 @@
 CREATE USER n8n_user WITH ENCRYPTED PASSWORD :'N8N_DB_PASSWORD';
 CREATE DATABASE n8n OWNER n8n_user;
 
--- sim database and user
+-- sim database and user  
 CREATE USER sim_user WITH ENCRYPTED PASSWORD :'SIM_DB_PASSWORD';
 CREATE DATABASE sim OWNER sim_user;
-EOF
+
+-- Enable extensions only in sim database
+\connect sim
+CREATE EXTENSION IF NOT EXISTS vector;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
